e1920c96da6c819dfa58125b9d3c0c037ef4a17a
Oskar <Oskar.herdenberg@gmail.com>
2022-02-12 00:03:15 +0100

    Another solution

---
+++ src/Yinyangstones.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Yinyangstones {
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        String input = console.nextLine();
+        int white = 0;
+        int black = 0;
+        for(int i = 0; i <input.length(); i++){
+            if(input.charAt(i) == 'W'){
+                white++;
+            }else {
+                black++;
+            }
+        }
+        System.out.println(black >= white+1 || white >= black+1 ? "0" : "1");
+    }
+}
